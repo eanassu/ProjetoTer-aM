ad061ac10e2d1f15eb47ba0b0a77eb34a9c5650a
052787 <052787@SPFAM2LAB143-03.famacad.com.br>
2022-05-03 10:42:05 -0300

    padrão MVC

--- Projeto/src/main/java/br/vemprafam/logica/Logica.java
+++
@@ -1,8 +0,0 @@
-package br.vemprafam.logica;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-abstract public class Logica {
-	abstract public String executar(HttpServletRequest request, HttpServletResponse response);
-}
--- Projeto/src/main/java/br/vemprafam/servlet/ServletCadastro.java
+++ Projeto/src/main/java/br/vemprafam/servlet/ServletCadastro.java
@@ -1,6 +1,9 @@
 package br.vemprafam.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,20 +12,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.vemprafam.logica.Logica;
-import br.vemprafam.logica.LogicaCadastro;
+import br.vemprafam.dao.DaoAluno;
+import br.vemprafam.pojo.Aluno;
 
 /**
- * Servlet implementation class ServletControle
+ * Servlet implementation class ServletCadastro
  */
-@WebServlet("/ServletControle")
-public class ServletControle extends HttpServlet {
+@WebServlet("/cadastrarAluno")
+public class ServletCadastro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletControle() {
+    public ServletCadastro() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,18 +34,23 @@ public class ServletControle extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String op = request.getParameter("op");
-		System.out.println(op);
-		String nomeClasse = "br.vemprafam.logica.Logica"+op;
+		int ra = Integer.parseInt(request.getParameter("ra"));
+		String nome = request.getParameter("nome");
+		String dataStr = request.getParameter("dataNascimento");
+		double renda = Double.parseDouble(request.getParameter("renda"));
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataNascimento = null;
 		try {
-			Class<?> classe = Class.forName(nomeClasse);
-			Logica logica = (Logica) classe.newInstance();
-			String pagina = logica.executar(request, response);
-			RequestDispatcher rd = request.getRequestDispatcher(pagina);
-			rd.forward(request, response);
-		} catch( Exception e ) {
+			dataNascimento = format.parse(dataStr);
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		String email = request.getParameter("email");
+		Aluno aluno = new Aluno(ra,nome,dataNascimento,renda,email);
+		DaoAluno dao = new DaoAluno();
+		dao.inserirAluno(aluno);
+		RequestDispatcher rd = request.getRequestDispatcher("/aluno-cadastrado.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
